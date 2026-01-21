121d11b8ae505d42d19698d438b4c1df85d43cee
Maria Eldridge <mveldridge13@gmail.com>
2026-01-22 08:42:30 +1000

    Helmet added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
+import helmet from "helmet";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Security headers
+  app.use(helmet());
 
   app.useGlobalPipes(
     new ValidationPipe({
