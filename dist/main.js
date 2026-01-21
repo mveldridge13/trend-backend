5d98aa9c99cc09128a0038323437d7b1fbaf3b00
Maria Eldridge <mveldridge13@gmail.com>
2026-01-22 08:42:30 +1000

    Helmet added

--- dist/main.js
+++ dist/main.js
@@ -3,9 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
+const helmet_1 = require("helmet");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use((0, helmet_1.default)());
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
         transform: true,
